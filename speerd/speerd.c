f838e94556c6ebe83353b2a253b5d50a1ba6621e
Paul Duffy <PDuffy@HomeServicesIowa.com>
2024-01-03 16:16:15 -0600

    Add project files.

---
+++ speerd/speerd.c
@@ -0,0 +1,52 @@
+// speerd.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+int main()
+{
+    char response;
+    const int bufferSize = 104857600; // 100 MB
+    char* buffer;
+    clock_t start, end;
+    double cpu_time_used;
+
+    buffer = malloc(bufferSize);
+    if (buffer == NULL)
+    {
+        perror("Error allocating memory");
+        return 1;
+    }
+
+    // Write to mem
+
+    start = clock();
+    memset(buffer, 0, bufferSize); // zero out buffer (fill buffer with zeros)
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Write time: %f seconds\n", cpu_time_used);
+    printf("Write speed: %f MB/s\n", bufferSize / (cpu_time_used * 104857600));
+
+    // Simulate reading from mem
+
+    volatile char temp; // volatile to prevent compiler optimizations
+    start = clock();
+    for (int i = 0; i < bufferSize; i++)
+    {
+        temp = buffer[i];
+    }
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Read time: %f seconds\n", cpu_time_used);
+    printf("Read speed: %f MB/s\n", bufferSize / (cpu_time_used * 104857600));
+
+    free(buffer);
+    return 0;
+
+}
