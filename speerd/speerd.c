19ff50d16be0e34365aa632229de6efbbbbcce06
Agate <psduffy@icloud.com>
2024-01-03 21:18:50 -0500

    Higher Resolution
    Using clock_gettime & CLOCK_MONOTONIC

--- speerd/speerd.c
+++ speerd/speerd.c
@@ -1,52 +1,48 @@
 // this version works with gcc
-//
+// compile with -lrt
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
+#define BILLION 1000000000L
 
-int main()
-{
-    char response; // not called in this version
-    const int bufferSize = 104857600; // 100 MB
+int main() {
+    const size_t bufferSize = 104857600; // 100 MB
     char* buffer;
-    clock_t start, end;
-    double cpu_time_used;
+    struct timespec start, end;
+    double write_time, read_time;
+    long sum = 0; // Used to prevent read loop optimization
 
-    buffer = malloc(bufferSize);
-    if (buffer == NULL)
-    {
+    buffer = (char*)malloc(bufferSize);
+    if (buffer == NULL) {
         perror("Error allocating memory");
         return 1;
     }
 
-    // Write to mem
+    // Write to memory
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    memset(buffer, 0, bufferSize);
+    clock_gettime(CLOCK_MONOTONIC, &end);
 
-    start = clock();
-    memset(buffer, 0, bufferSize); // zero out buffer (fill buffer with zeros)
-    end = clock();
+    write_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / (double)BILLION;
+    printf("Write time: %f seconds\n", write_time);
+    printf("Write speed: %f MB/s\n", (bufferSize / (1024.0 * 1024.0)) / write_time);
 
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Write time: %f seconds\n", cpu_time_used);
-    printf("Write speed: %f MB/s\n", bufferSize / (cpu_time_used * 104857600));
-
-    // Simulate reading from mem
-
-    volatile char temp; // volatile to prevent compiler optimizations
-    start = clock();
-    for (int i = 0; i < bufferSize; i++)
-    {
-        temp = buffer[i];
+    // Read from memory in blocks
+    size_t blockSize = 4096; // 4 KB blocks
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (size_t i = 0; i < bufferSize; i += blockSize) {
+        sum += buffer[i]; // Simple operation to prevent optimization
     }
-    end = clock();
+    clock_gettime(CLOCK_MONOTONIC, &end);
 
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Read time: %f seconds\n", cpu_time_used);
-    printf("Read speed: %f MB/s\n", bufferSize / (cpu_time_used * 104857600));
+    read_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / (double)BILLION;
+    printf("Read time: %f seconds\n", read_time);
+    printf("Read speed: %f MB/s\n", (bufferSize / (1024.0 * 1024.0)) / read_time);
 
     free(buffer);
+    printf("Sum: %ld\n", sum); // Print sum to ensure the read loop is not optimized out
     return 0;
-
 }
